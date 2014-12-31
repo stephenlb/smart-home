4617745a5c972be1d96199df66b19ca5014eab3c
Stephen Blum <stephen@pubnub.com>
2014-12-31 00:48:33 -0800

    I'm so fancy.

---
+++ js/smart-home.js
@@ -0,0 +1,14 @@
+(function(){
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+setTimeout( function() {
+    document.getElementById("octo-group").className    = " ";
+    document.getElementById("numbers-group").className = " ";
+    document.getElementById("labels-group").className  = " ";
+}, 300 );
+
+
+})();
