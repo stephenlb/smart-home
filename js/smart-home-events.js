740b1d0af711bea17a56e6dd2909939b77870171
Stephen Blum <stephen@pubnub.com>
2015-01-03 02:24:31 -0800

    added auto-slider awesome exellence and PubNub mgs.

--- js/smart-home-events.js
+++ js/smart-home-events.js
@@ -1,15 +1,46 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // SHOW LEDs Menu
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+var led_transmitter = null
+,   led_id          = 0;
+
 PUBNUB.events.bind( 'click.leds', function(event) {
     console.log('LEDs');
 
     // Show Sub Menu (Lights/LEDs)
     show_submenu('light-menu');
 
+    // Start Signal Emitter
+    //clearInterval(led_transmitter);
+    //led_transmitter = setInterval( transmit_led, 500 );
+
     // Hide Main Menu
     hide_menu(event);
 
+} );
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Transmit LED Settings
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+var transmit_led = PUBNUB.updater( function() {
+    PUBNUB.events.fire( 'send-iot-signal', {
+        ledID          : +led_id,
+        minPulseLength : +PUBNUB.$('min-pulse').value,
+        maxPulseLength : +PUBNUB.$('max-pulse').value,
+        waitFloor      : +PUBNUB.$('min-wait').value,
+        waitCeiling    : +PUBNUB.$('max-wait').value
+    } );
+}, 500 );
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// LED SLIDER Settings
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+var ledlights = PUBNUB.$('light-menu').getElementsByTagName('input');
+PUBNUB.each( ledlights, function(light) {
+    PUBNUB.bind( 'touchmove,mousemove', light, function(e) {
+        transmit_led();
+        return true;
+    } );
 } );
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -41,7 +72,12 @@ PUBNUB.events.bind( 'click.light-select', function(event) {
     console.log( 'LIGHT SELECT', event );
 
     var lights = PUBNUB.$('light-selector').getElementsByTagName('div');
+
     play('click-sound');
+
+    // Set the targeted LED
+    led_id = +event.data;
+    transmit_led();
 
     PUBNUB.each( lights, function(light) {
         if (PUBNUB.attr( light, 'data-value' ) == event.data)
@@ -49,6 +85,13 @@ PUBNUB.events.bind( 'click.light-select', function(event) {
         else
             light.className = "light-select light-unselected";
     } );
+} );
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// LIGHT Ragne Selectors
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+PUBNUB.events.bind( 'click.light-slider', function(event) {
+    console.log(event);
 } );
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -76,16 +119,18 @@ function hide_all_submenus() {
 function hide_menu(event) {
     console.log(event);
 
+
     play('click-beep-sound');
 
     if (hide_menu.invisible) {
         hide_menu.invisible = false;
-        setTimeout( function(){ PUBNUB.events.fire('show-menu') }, 300 );
+        setTimeout( function(){ PUBNUB.events.fire('show-menu') }, 1 );
+        //clearInterval(led_transmitter);
         return hide_all_submenus();
     }
     else {
         hide_menu.invisible = true;
-        setTimeout( function(){ PUBNUB.events.fire('hide-menu') }, 300 );
+        setTimeout( function(){ PUBNUB.events.fire('hide-menu') }, 1 );
     }
 
     0&&animate( PUBNUB.$("octo-"+event.data+"-indicator"), [
--- js/smart-home-pubnub.js
+++ js/smart-home-pubnub.js
@@ -3,13 +3,37 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // PubNub Config
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+var settings = {
+    channel       : "futureChannel",
+    publish_key   : 'demo-36',
+    subscribe_key : 'demo-36'
+};
 
-var pubnub = PUBNUB({ publish_key : 'demo-36', subscribe_key : 'demo-36' });
+var pubnub = PUBNUB(settings);
 
-
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Publish Data
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 PUBNUB.events.bind( 'send-iot-signal', function(data) {
-    console.log('durpaksldjf PUBLSIH HERE>...', data );
+    console.log( 'PUBLISHING', data );
+    pubnub.publish({ channel : settings.channel, message : data });
 } );
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Listen for Data
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+pubnub.subscribe({
+    channel : settings.channel,
+    message : receiver
+});
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Data Receiver
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function receiver(data) {
+    console.log( 'RECEIVING', data );
+    PUBNUB.events.fire( 'receive-iot-signal', data );
+}
 
 
 
